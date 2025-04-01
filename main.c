9718653cec0c639f4f3daf4b2f8605f16238175a
Robert Popovici <robert.popovici57@gmail.com>
2025-04-01 23:56:22 +0300

    Add treasures in progress

--- main.c
+++ main.c
@@ -6,46 +6,80 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 
 
-int narg;
-char **args;
+
 mode_t mode = S_IRWXO | S_IRWXG | S_IRWXU;
 
 typedef struct GPSCoordinates
 {
     float latitude;
     float longitude;
-};
+}GPSCoordinates;
 
 typedef struct treasure
 {
-    char treasureId[100];
+    char treasureId[20];
     char userName[20];
-    struct GPSCoordinates coordinates;
-    char clueText[100];
+    //GPSCoordinates coordinates;
+    char clueText[50];
     int Value;
-};
+}treasure;
+treasure getTreasureInfo()
+{
+    treasure t;
 
+    printf("Treasure Id:");
+    scanf("%s", t.treasureId);
+    printf("User name:");
+    scanf("%s", t.userName);
+    
+    // printf("Latitude:");
+    // scanf("%f", &t.coordinates.latitude);
+    // printf("longitude:");
+    // scanf("%f", &t.coordinates.longitude);
+    printf("Clue Text:");
+    scanf("%s", t.clueText);
+    printf("Value:");
+    scanf("%d", &t.Value);
+    return t;
+}
 void add_treasure(char *huntId)
 {
     
     DIR *dirp = NULL;
-    char path[1000] = "./";
-    strcat(path, huntId);
+    int fd;
+    char path[2000] = "";
+    sprintf(path, "./%s", huntId);
     dirp = opendir(path);
     if (dirp == NULL)
     {
-        mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        dirp = opendir(path);
+        mkdir(path,mode);
+        
     }
     else
     {
         printf("Exista directorul");
         closedir(dirp);
     }
-    // daca nu exista hunt creeam hunt cu id= huntId
+     
+    char treasureFile[1000] = "";
+    sprintf(treasureFile, "%s_treasures.dat", huntId);
+    sprintf(path,"./%s/%s", huntId, treasureFile);
+    
+    fd = open(path, O_RDWR | O_APPEND | O_CREAT , mode);
+    
+    treasure tr = getTreasureInfo();
+
+    write(fd, &tr, sizeof(treasure));
+    treasure t2;
+    int res=read(fd, &t2, sizeof(treasure));
+    
+    printf("=====%d===%d===", res, t2.Value);
+    close(fd);
 }
+
 
 void remove_treasure(char *huntId, char *treasureId)
 {
@@ -54,12 +88,11 @@ void remove_treasure(char *huntId, char *treasureId)
 void remove_hunt(char *huntId)
 {
 
-    char path[1000] = "./";
-    char extcmd[1000] = "rm -rf ";
-    strcat(path, huntId);
-    strcat(extcmd,path);
-    strcat(extcmd,"/*");
-    system(extcmd);
+    char path[1000] = "";
+    sprintf(path, "./%s", huntId);
+    char extcmd[2000] = "rm -rf ";
+    sprintf(extcmd, "rm -rf %s/*", path);
+    system(extcmd); // stergem directorul recursiv
 
     if(rmdir(path)==0)
     {
@@ -86,8 +119,7 @@ int main(int argc, char **argv)
         printf("Argumente insuficiente!");
         exit(-1);
     }
-    narg = argc;
-    args = argv;
+    
     if (strcmp(argv[1], "--add") == 0)
     {
         if(argc ==1)
@@ -124,4 +156,5 @@ int main(int argc, char **argv)
     {
         printf("Comanda inexistenta");
     }
+    return 0;
 }
