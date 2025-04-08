1a5625a58bba402fef18118c9a51fcf43b7f186a
Robert Popovici <robert.popovici57@gmail.com>
2025-04-08 23:39:13 +0300

    Updated README and added input validation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# TreasureManager
- OS Project
+
+# Treasure Manager
+
+OS Project Phase 1
+
+
+
+## How to run
+To make the program
+```c
+gcc -o treasure_manager main.c
+```
+To add a treasure 
+```c
+./treasure_manager --add <huntId>
+```
+To list a hunt
+```c
+./treasure_manager --list <huntId>
+```
+To view a treasure from a hunt
+```c
+./treasure_manager --view <huntId> <treasureId>
+```
+To remove a hunt 
+```c
+./treasure_manager --remove <huntId>
+```
+To remove a treasure from a hunt
+```c
+./treasure_manager --remove <huntId> <treasureId>
+```
+
+
+    
--- main.c
+++ main.c
@@ -11,7 +11,9 @@
 #include <time.h>
 #include <ctype.h>
 
-#define PATH_LENGTH 256
+#define PATH_LENGTH 512
+#define MSJ_LENGTH 128
+#define DEFAULT_LENGTH 32
 
 mode_t mode = S_IRWXO | S_IRWXG | S_IRWXU;
 
@@ -23,10 +25,10 @@ typedef struct GPSCoordinates
 
 typedef struct treasure
 {
-    char treasureId[20];
-    char userName[20];
+    char treasureId[DEFAULT_LENGTH];
+    char userName[DEFAULT_LENGTH];
     GPSCoordinates coordinates;
-    char clueText[20];
+    char clueText[DEFAULT_LENGTH];
     int Value;
 } treasure;
 
@@ -36,23 +38,108 @@ treasure getTreasureInfo()
     memset(&t, 0, sizeof(treasure));
 
     printf("Treasure Id:");
-    fgets(t.treasureId, 20, stdin);
-    t.treasureId[strlen(t.treasureId) - 1] = 0;
+    if (fgets(t.treasureId, DEFAULT_LENGTH, stdin) == NULL)
+    {
+        perror("Error reading Treasure Id");
+        exit(-1);
+    }
+    
+    if (t.treasureId[strlen(t.treasureId) - 1] != '\n')
+    {
+        printf("Input too long for Treasure Id.");
+        exit(-1);
+    }
+    t.treasureId[strlen(t.treasureId) - 1] = '\0'; 
+
     printf("User name:");
-    fgets(t.userName, 20, stdin);
-    t.userName[strlen(t.userName) - 1] = 0;
+    if (fgets(t.userName, DEFAULT_LENGTH, stdin) == NULL)
+    {
+        perror("Error reading User name");
+        exit(-1);
+    }
+    if (t.userName[strlen(t.userName) - 1] != '\n')
+    {
+        printf("Input too long for User name.");
+        exit(-1);
+    }
+    t.userName[strlen(t.userName) - 1] = '\0';
+
+    char buffer[100];
+    char *endptr;
+
     printf("Latitude:");
-    scanf("%f", &t.coordinates.latitude);
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+    {
+        perror("Error reading Latitude");
+        exit(-1);
+    }
+    if (buffer[strlen(buffer) - 1] != '\n')
+    {
+        printf("Input too long for Latitude.\n");
+        exit(-1);
+    }
+    buffer[strlen(buffer) - 1] = '\0';
+    t.coordinates.latitude = strtof(buffer, &endptr);
+    if (*endptr != '\0')
+    {
+        printf("Invalid input. Latitude must be a float.\n");
+        exit(-1);
+    }
+
     printf("Longitude:");
-    scanf("%f", &t.coordinates.longitude);
-    getc(stdin);
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+    {
+        perror("Error reading Longitude");
+        exit(-1);
+    }
+    if (buffer[strlen(buffer) - 1] != '\n')
+    {
+        printf("Input too long for Longitude.\n");
+        exit(-1);
+    }
+    buffer[strlen(buffer) - 1] = '\0';
+    t.coordinates.longitude = strtof(buffer, &endptr);
+    if (*endptr != '\0')
+    {
+        printf("Invalid input. Longitude must be a float.\n");
+        exit(-1);
+    }
+
     printf("Clue Text:");
-    fgets(t.clueText, 20, stdin);
-    t.clueText[strlen(t.clueText) - 1] = 0;
+    if (fgets(t.clueText, DEFAULT_LENGTH, stdin) == NULL)
+    {
+        perror("Error reading Clue Text");
+        exit(-1);
+    }
+    if (t.clueText[strlen(t.clueText) - 1] != '\n')
+    {
+        printf("Input too long for Clue Text. Maximum length is %d.\n", DEFAULT_LENGTH - 1);
+        exit(-1);
+    }
+    t.clueText[strlen(t.clueText) - 1] = '\0';
+
     printf("Value:");
-    scanf("%d", &t.Value);
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+    {
+        perror("Error reading Value");
+        exit(-1);
+    }
+    if (buffer[strlen(buffer) - 1] != '\n')
+    {
+        printf("Input too long for Value.\n");
+        exit(-1);
+    }
+    buffer[strlen(buffer) - 1] = '\0';
+    t.Value = strtol(buffer, &endptr, 10);
+    if (*endptr != '\0')
+    {
+        printf("Invalid input. Value must be an integer.\n");
+        exit(-1);
+    }
+
     return t;
 }
+
 void add_to_log(char *huntId, char *msj)
 {
     if (msj == NULL)
@@ -60,7 +147,7 @@ void add_to_log(char *huntId, char *msj)
         exit(-1);
     }
     int fd;
-    char path[PATH_LENGTH] = "", msjj[2 * PATH_LENGTH] = "";
+    char path[PATH_LENGTH] = "", msjj[MSJ_LENGTH] = "";
     if (sprintf(path, "./Game/%s/loggedhunt", huntId) < 0)
     {
         perror("Error creating log file path:add_to_log");
@@ -107,19 +194,43 @@ void print_treasure(treasure t)
 {
     printf("Treasure ID:%s\nUsername:%s\nLatitude:%.2f\nLongitude:%.2f\nClue Text:%s\nValue:%d\n\n", t.treasureId, t.userName, t.coordinates.latitude, t.coordinates.longitude, t.clueText, t.Value);
 }
-void add_treasure(char *huntId)
+int hunt_exists(char *huntId)
 {
-    DIR *dirp = NULL;
-    int fd;
-    char path[2 * PATH_LENGTH] = "", msj[PATH_LENGTH] = "", lpath[PATH_LENGTH] = "", treasureFile[PATH_LENGTH] = "";
-    treasure tr;
+    char path[PATH_LENGTH] = "";
+    DIR *dir;
     if (sprintf(path, "./Game/%s", huntId) < 0)
     {
-        perror("Error creating directory path:add_treasure");
+        perror("Error making path:remove_treasure");
         exit(-1);
     }
-    if ((dirp = opendir(path)) == NULL)
+    if ((dir = opendir(path)) == NULL)
     {
+        return 0;
+    }
+    else
+    {
+        if (closedir(dir) == -1)
+        {
+            perror("Error closing dir:remove_treasure");
+            exit(-1);
+        }
+        return 1;
+    }
+}
+void add_treasure(char *huntId)
+{
+    int fd;
+    char path[PATH_LENGTH] = "", msj[MSJ_LENGTH] = "", lpath[PATH_LENGTH] = "", treasureFile[PATH_LENGTH] = "";
+    treasure tr;
+    tr = getTreasureInfo();
+    
+    if (hunt_exists(huntId)==0)
+    {
+        if (sprintf(path, "./Game/%s", huntId) < 0)
+        {   
+            perror("Error making path:remove_treasure");
+            exit(-1);
+        }
         if (mkdir(path, mode) == -1)
         {
             perror("Error creating directory:add_treasure");
@@ -157,15 +268,7 @@ void add_treasure(char *huntId)
             exit(-1);
         }
     }
-    else
-    {
-        if (closedir(dirp) == -1)
-        {
-            perror("Error closing dir:add_treasure");
-            exit(-1);
-        }
-    }
-
+    
     if (sprintf(treasureFile, "%s_treasures.dat", huntId) < 0)
     {
         perror("Error creating treasure file name:add_treasure");
@@ -183,7 +286,6 @@ void add_treasure(char *huntId)
         exit(-1);
     }
 
-    tr = getTreasureInfo();
 
     if (write(fd, &tr, sizeof(treasure)) == -1)
     {
@@ -195,7 +297,7 @@ void add_treasure(char *huntId)
         perror("Error closing treasures file:add_treasure");
         exit(-1);
     }
-    if (sprintf(msj, "Treasure with id:%s was added.\n", tr.treasureId) < 0)
+    if (sprintf(msj, "Treasure with id:%s was added.", tr.treasureId) < 0)
     {
         perror("Error making log message:add_treasure");
         exit(-1);
@@ -204,27 +306,13 @@ void add_treasure(char *huntId)
 }
 void remove_treasure(char *huntId, char *treasureId)
 {
-    char path[2 * PATH_LENGTH] = "", msj[PATH_LENGTH] = "";
-    DIR *dir;
+    char path[ PATH_LENGTH] = "", msj[MSJ_LENGTH] = "";
     int fd, fdw, found = 0, pos = 0, res;
     treasure t;
-    if (sprintf(path, "./Game/%s", huntId) < 0)
+    if(hunt_exists(huntId)==0)
     {
-        perror("Error making path:remove_treasure");
+        printf("Hunt with id:%s not found\n", huntId);
         exit(-1);
-    }
-    if ((dir = opendir(path)) == NULL)
-    {
-        perror("Error finding hunt:remove_treasure");
-        exit(-1);
-    }
-    else
-    {
-        if (closedir(dir) == -1)
-        {
-            perror("Error closing dir:remove_treasure");
-            exit(-1);
-        }
     }
     if (sprintf(path, "./Game/%s/%s_treasures.dat", huntId, huntId) < 0)
     {
@@ -306,7 +394,7 @@ void remove_treasure(char *huntId, char *treasureId)
 void remove_rec(char *path)
 {
     struct dirent *dp;
-    char spath[5 * PATH_LENGTH];
+    char spath[ PATH_LENGTH];
     DIR *d;
     struct stat st;
 
@@ -370,23 +458,17 @@ void remove_rec(char *path)
 void remove_hunt(char *huntId)
 {
 
-    char path[2 * PATH_LENGTH] = "";
-    DIR *d;
-    if (sprintf(path, "./Game/%s", huntId) < 0)
+    char path[ PATH_LENGTH] = "";
+    
+    if (hunt_exists(huntId)==0)
     {
-        perror("Error making path:list");
-        exit(-1);
-    }
-
-    if ((d = opendir(path)) == NULL)
-    {
-        perror("Error finding hunt:remove_hunt");
+        printf("Hunt with id:%s not found",huntId);
     }
     else
     {
-        if (closedir(d) == -1)
+        if (sprintf(path, "./Game/%s", huntId) < 0)
         {
-            perror("Error closing dir:remove_hunt");
+            perror("Error making path:list");
             exit(-1);
         }
         remove_rec(path);
@@ -405,28 +487,14 @@ void remove_hunt(char *huntId)
 void list(char *huntId)
 {
     struct stat st;
-    DIR *d;
-    char path[PATH_LENGTH] = "", msj[PATH_LENGTH] = "";
+    char path[PATH_LENGTH] = "", msj[MSJ_LENGTH] = "";
     int fd, res = 0;
     treasure t;
 
-    if (sprintf(path, "./Game/%s", huntId) < 0)
+    if(hunt_exists(huntId)==0)
     {
-        perror("Error making path:list");
+        printf("Hunt with id:%s not found",huntId);
         exit(-1);
-    }
-    if ((d = opendir(path)) == NULL)
-    {
-        perror("Error finding hunt:list");
-        exit(-1);
-    }
-    else
-    {
-        if (closedir(d) == -1)
-        {
-            perror("Error closing dir:list");
-            exit(-1);
-        }
     }
 
     if (sprintf(path, "./Game/%s/%s_treasures.dat", huntId, huntId) < 0)
@@ -439,7 +507,7 @@ void list(char *huntId)
         perror("Error using stat:list");
         exit(-1);
     }
-    printf("Hunt Id:%s\nFile Size:%ld bytes\nLast modification:%s\n", huntId, st.st_size, ctime(&st.st_mtime));
+    printf("Hunt Id:%s\nFile Size:%lld bytes\nLast modification:%s\n", huntId, st.st_size, ctime(&st.st_mtime));
     if ((fd = open(path, O_RDONLY, mode)) < 0)
     {
         perror("Error opening treasures file:list");
@@ -462,7 +530,7 @@ void list(char *huntId)
         perror("Error closing treasures file:list");
         exit(-1);
     }
-    if (sprintf(msj, "Hunt with id:%s was listed.\n", huntId) < 0)
+    if (sprintf(msj, "Hunt with id:%s was listed.", huntId) < 0)
     {
         perror("Error making log message:list");
         exit(-1);
@@ -471,28 +539,15 @@ void list(char *huntId)
 }
 void view(char *huntId, char *treasureId)
 {
-    char path[2 * PATH_LENGTH] = "", msj[PATH_LENGTH] = "";
-    DIR *dir;
+    char path[ PATH_LENGTH] = "", msj[MSJ_LENGTH] = "";
+
     treasure tr;
     int found = 0, fd, res;
 
-    if (sprintf(path, "./Game/%s", huntId) < 0)
+    if(hunt_exists(huntId)==0)
     {
-        perror("Error making path:view");
+        printf("Hunt with id:%s not found",huntId);
         exit(-1);
-    }
-    if ((dir = opendir(path)) == NULL)
-    {
-        perror("Error finding hunt:view");
-        exit(-1);
-    }
-    else
-    {
-        if (closedir(dir) == -1)
-        {
-            perror("Error closing dir:view");
-            exit(-1);
-        }
     }
     if (sprintf(path, "./Game/%s/%s_treasures.dat", huntId, huntId) < 0)
     {
@@ -538,7 +593,7 @@ void view(char *huntId, char *treasureId)
 }
 
 int main(int argc, char **argv)
-{ /*
+{ 
      if (argc == 1)
      {
          printf("Not enough arguments!\n");
@@ -610,16 +665,6 @@ int main(int argc, char **argv)
      {
          printf("Unknown command\n");
      }
-         */
-    char buffer[100];
-    double value;
-    char *endptr;
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
-        return -1; /* Unexpected error */
-    value = strtod(buffer, &endptr);
-    if ((*endptr == '\0') || (isspace(*endptr) != 0))
-        printf("It's float: %f\n", value);
-    else
-        printf("It's NOT float ...\n");
-    return 0;
+         
+   
 }
